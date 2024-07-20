9c1a89312d51c8c008bf2c1ee435fb5f93cf296f
Your Name <you@example.com>
2024-07-20 13:49:41 -0400

    DC

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -331,11 +331,6 @@ export default function BottomNavigation({ rol }) {
                     </Link>
                 </div>
 
-
-
-
-
-
                     <Link href='/SolucionesIT' onClick={() => setNav(false)}>
                         <h3 className="text-black text-[12px] font-medium pt-2 pl-5 m-0 bg-[#F1BA06]  border border-[#ffffff38] text-center px-[5px] py-[5px] rounded-[7px] mx-[20px] my-[15px]">{languaje === 'Español' ? 'SOLUCIONES IT' : 'IT SOLUTIONS'}</h3>
                     </Link>
